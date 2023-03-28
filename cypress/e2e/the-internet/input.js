4c5087561ed29ae829cc6d77a9d878f245069277
Jose Ismael <ismaeldg01@gmail.com>
2023-03-28 02:07:20 -0400

    new pages an tests

--- cypress/e2e/the-internet/input.js
+++ cypress/e2e/the-internet/input.js
@@ -2,17 +2,44 @@ import InputsPage from "../../support/pageObjects/inputsPage";
 
 const inputspage = new InputsPage;
 
-const digit = Math.floor(Math.random() * 100000000000000000000000000000000000);
+const positiveNumber = Math.floor(Math.random() * 10000000000000000000);
+const negativeNumber = Math.floor(Math.random() * -10000000000000000000);
+const RandExp = require('randexp');
+const rgx = /([^eA-Z0-9]{1})/
+const randomLetter = new RandExp(rgx).gen();
+const rgx1 = /([e])/
+const letterE = new RandExp(rgx1).gen();
 
 describe("testing the input page", () => {
 
 
-    // beforeEach(function(){
-    //     cy.openInputsPage();
-    // });
-
-    it("Entrando un numero", () => {
+    beforeEach(function(){
         cy.openInputsPage();
-        inputspage.inputBox().type(digit);
-    })
+    });
+
+    it("inputing positive number", () => {
+        inputspage.inputBox().click();
+        inputspage.inputBox().should("have.focus");
+        inputspage.inputBox().type(positiveNumber);
+    });
+
+    it("inputing negative number", () => {
+        inputspage.inputBox().click();
+        inputspage.inputBox().should("have.focus");
+        inputspage.inputBox().type(negativeNumber);
+    });
+
+    it("inputing letters that are not the letter e", () => {
+        inputspage.inputBox().click();
+        inputspage.inputBox().should("have.focus");
+        inputspage.inputBox().type(randomLetter);
+    });
+
+    it("inputing the letter 'e' more than once", () => {
+        inputspage.inputBox().click();
+        inputspage.inputBox().should("have.focus");
+        inputspage.inputBox().type(letterE);
+    });
+
+    // it()
 })
---
+++ cypress/e2e/the-internet/smokeTests.js
@@ -0,0 +1,55 @@
+import LoginPage from "../../support/pageObjects/loginPage";
+import InputsPage from "../../support/pageObjects/inputsPage";
+import HoversPage from "../../support/pageObjects/hoversPage";
+import DropdownPage from "../../support/pageObjects/dropdownPage";
+
+
+const loginPage = new LoginPage();
+const inputsPage  = new InputsPage();
+const hoversPage = new HoversPage();
+const dropdownPage = new DropdownPage;
+
+describe("Smoke tests", () => {
+
+
+    it("Validating in loginPage", () =>{
+
+        cy.openLoginPage();
+        cy.url().should("eq", Cypress.env("loginUrl"));
+        loginPage.pageTitle().should("contains.text", "Login Page");
+        loginPage.userNameInput().should("be.visible");
+        loginPage.passwordInput().should("be.visible");
+    });
+
+    it("Validating in inputsPage", () => {
+
+        cy.openInputsPage();
+        cy.url().should("eq", Cypress.env("inputsUrl"));
+        inputsPage.pageTitle().should("be.visible");
+        inputsPage.inputBox().should("be.visible");
+    });
+
+    it("Validating in hoversPage", () => {
+
+        cy.openHoversPage();
+        cy.url().should("eq", Cypress.env("hoversUrl"));
+        hoversPage.pageTitle().should("contains.text", "Hovers");
+        hoversPage.firstImage().should("be.visible");
+        hoversPage.secondImage().should("be.visible");
+        hoversPage.thirdImage().should("be.visible");
+    });
+
+    it("Validating in dropdownPage", () => {
+
+        cy.openDropdownPage();
+        cy.url().should("eq", Cypress.env("dropdownUrl"));
+        dropdownPage.pageTitle().should("contains.text", "Dropdown List");
+        dropdownPage.dropdown().should("be.visible");
+    });
+
+   
+
+
+
+
+})
